e3aac5e2a755c7eeb21a95e6e15ccbeae8deec61
Jim Nelson <jim@jimnelson.us>
2022-02-12 15:31:09 -0800

    Building markov chain

--- python/report-to-json.py
+++ python/report-to-json.py
@@ -11,6 +11,22 @@ def main(inpath, outpath):
         if filename.endswith(".xlsx"):
             df = pd.read_excel('../data/' + filename)
             print(df)
-            df.to_json("../data/" + filename[0:-4] + ".json", orient="records", indent=2)
+            df.to_json("../data/" + filename[0:-5] + ".json", orient="records", indent=2)
 
-main(xlsx_inpath, json_outpath)
+def make_markov():
+    import os
+    markov_dict = {}
+    with open("words.txt") as file:
+        for word in file.readlines():
+            print(word.strip())
+            word = word.strip().lower()
+            for i in range(0, len(word) - 1):
+                if not word[i] in markov_dict:
+                    markov_dict[word[i]] = [word[i+1]]
+                else:
+                    markov_dict[word[i]].append(word[i+1])
+    return markov_dict
+
+foo = make_markov()
+print(foo)
+# main(xlsx_inpath, json_outpath)
