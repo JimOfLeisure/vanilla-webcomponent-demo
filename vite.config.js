ae45e33855a1924feb538e30a477d4518faeb896
Jim Nelson <jim@jimnelson.us>
2022-02-15 21:04:32 -0800

    added vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+export default {
+    build : {
+        // For github pages, leave it readable
+        minify: false,
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        },
+    },
+}
