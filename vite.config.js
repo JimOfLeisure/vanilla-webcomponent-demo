bcdfebca6e2a12fea31dbdbba66932375ff33617
Jim Nelson <jim@jimnelson.us>
2022-02-15 21:32:01 -0800

    changing build dir to docs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ export default {
     build : {
         // For github pages, leave it readable
         minify: false,
+        // For GitHub Pages, put in docs/ folder
+        outDir: "docs",
+        // This seems to be the way to avoid hashed filenames
         rollupOptions: {
             output: {
                 entryFileNames: `assets/[name].js`,
