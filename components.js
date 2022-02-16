6126fe45bf9e0e70d8a5100a668ca0d3154fab71
Jim Nelson <jim@jimnelson.us>
2022-02-15 20:30:02 -0800

    Added style; reformatted data

--- components.js
+++ components.js
@@ -10,12 +10,12 @@ export class SeoClusterApp extends HTMLElement {
               "Volume Category" : "",
               "Number of Pages" : 0
             }];
-        let xhr = new XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
         xhr.onload = () => {
             if (xhr.status >= 200 && xhr.status < 300) {
                 this.seoClusterData = JSON.parse(xhr.responseText);
-                console.log(xhr.responseText);
-                console.log(this.seoClusterData);
+                // console.log(xhr.responseText);
+                // console.log(this.seoClusterData);
                 this.render();
             } else {
                 console.log(xhr);
@@ -30,9 +30,12 @@ export class SeoClusterApp extends HTMLElement {
         this.render();
     }
     render() {
-        this.innerHTML = "SEO Cluster App";
+        this.innerHTML = "";
+        const title = document.createElement("h2");
+        title.innerHTML = "SEO Cluster App";
+        this.appendChild(title);
         this.seoClusterData.forEach(e => {
-            let el = document.createElement('seo-cluster');
+            const el = document.createElement('seo-cluster');
             el.setData(e);
             this.appendChild(el);
         });
@@ -40,13 +43,37 @@ export class SeoClusterApp extends HTMLElement {
 }
 export class SeoCluster extends HTMLElement {
     connectedCallback() {
-        this.render();
+        // this.render();
     }
     setData(data) {
         this.data = data;
         this.render();
     }
     render() {
-        this.innerHTML = this.data["Cluster Name"] + " " + this.data["Cluster Members"].length;
+        // this.innerHTML = this.data["Cluster Name"] +
+        //     " " + this.data["Cluster Members"].length +
+        //     " " + this.data["Competition Level"] +
+        //     " " + this.data["Volume Category"] +
+        //     " " + this.data["Number of Pages"]            
+        // ;
+        const title = document.createElement("h3");
+        title.innerText = this.data["Cluster Name"];
+        this.appendChild(title);
+        const infoList = document.createElement("ul");
+        this.appendChild(infoList);
+        [
+            this.data["Cluster Members"].length,
+            this.data["Competition Level"],
+            this.data["Volume Category"],
+            this.data["Number of Pages"]
+        ].forEach(text => {
+            const li = document.createElement("li");
+            li.innerText = text;
+            infoList.appendChild(li);
+        });
+        // "Cluster Members" - Expandable list widget
+        //    perhaps with links, filtering, sorting, scrolling
+        //  "Cluster Tree" - Monospace text based on current data
+        //    preferably hierarchical structured data
     }
 }
